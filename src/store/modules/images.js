67609849949ab8c0d0e7624fb77344ee08808727
John Kang <hphk.john@gmail.com>
2020-06-25 17:00:46 +0900

    add comment about async await

--- src/store/modules/images.js
+++ src/store/modules/images.js
@@ -16,24 +16,28 @@ const mutations = {
 const actions = {
   // 이 함수는 비동기 동작을 포함하고 있음!
   async fetchImages ({ rootState, commit }) {
-    const fullUrl = 'https://api.imgur.com/3/account/me/images';
+    const fullUrl = 'https://api.imgur.com/3/account/mse/images';
     const config = {
       headers: {
         Authorization: `Bearer ${rootState.auth.token}`
       }
     };
 
+    // 1
     try {
-      // 바로 여기가 비동기 파트다. 고로 기다려!
+      // 바로 여기가 비동기 파트다. (await) 고로 기다려!
       const res = await axios.get(fullUrl, config);
       commit('setImages', res.data.data);
     } catch (err) {
-      console.error(err.resposne)
+      console.error(err.response)
     }
 
+    // 2
     // axios.get(fullUrl, config)
     //   .then(res => commit('setImages', res.data.data))
     //   .catch(err => console.error(err.response))
+
+    // 1 & 2는 같은 코드입니다.
   },
   uploadImages() {},
 };
